6c0d0a623faf077078de5b5096a09cb26d6a85a4
Pikalova Anna <g2012.pikalova.a.12176@gmail.com>
2021-02-15 20:52:55 +0300

    add project

---
+++ Problems/Digits from 0 to 9 in a single line/src/Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        for (int i = 9; i >= 0; i--){
+            if (i == 0) {
+                System.out.print(i);
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
